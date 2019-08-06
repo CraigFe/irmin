54142e7a3ff8aa32931f05e0fb3ea8fc8aec138a
Craig Ferguson <me@craigfe.io>
2019-08-06 12:37:10 +0200

    irmin-pack: add missing Dict copyright header / interface

--- src/irmin-pack/pack_dict.mli
+++ src/irmin-pack/pack_dict.mli
@@ -10,15 +10,4 @@
    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. *)
 
-include Dict.Make (IO.Unix)
-
-(* Add IO caching around Dict.v *)
-let (`Staged v) =
-  let v_no_cache ~fresh ~readonly = v ~fresh ~readonly in
-  IO.with_cache ~clear
-    ~v:(fun capacity ~fresh ~shared:_ ~readonly ->
-      v_no_cache ~fresh ~readonly ~capacity)
-    "store.dict"
-
-let v ?fresh ?readonly ?(capacity = 100_000) root =
-  v capacity ?fresh ?readonly root
+include Dict.S
